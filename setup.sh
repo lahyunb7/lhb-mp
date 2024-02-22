6c62746f5e4fe9ff4db918b647f7184b72004fbd
lahyunb7 <lahyun7@gmail.com>
2024-02-22 03:49:58 +0000

    v2

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# nvm 설정 로드
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # 이 명령으로 nvm을 로드합니다.
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # 이 명령으로 nvm bash_completion을 로드합니다.
+
+# Node.js 16 버전 설치
+nvm install 16
+
+# npm 패키지 설치
+npm install --save-dev babel-loader @babel/core @babel/preset-env
+npm install --save-dev html-webpack-plugin
+npm install -g http-server
+npm install webpack webpack-cli --save-dev
+npm run build
+http-server
